b52b2d21c2346fcb6e7068fa56d4af3247be6361
Andreas Menzel <mail@andreas-menzel.com>
2023-03-19 14:34:07 +0100

    Check flying state in None, OnGround and Hovering

--- code/ADDS-DJI/app/src/main/java/com/andreasmenzel/adds_dji/OperationModes/None.java
+++ code/ADDS-DJI/app/src/main/java/com/andreasmenzel/adds_dji/OperationModes/None.java
@@ -1,8 +1,15 @@
 package com.andreasmenzel.adds_dji.OperationModes;
 
+import static com.andreasmenzel.adds_dji.OperationModes.OperationMode.States.attempting;
+
 import androidx.annotation.NonNull;
 
+import com.andreasmenzel.adds_dji.Managers.DJIManager;
+
 import org.greenrobot.eventbus.EventBus;
+
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
 
 /**
  * This Operation Mode is used if the current Operation Mode is not known. This mode tries to infer
@@ -23,7 +30,23 @@ public class None extends OperationMode {
 
     @Override
     public void perform(@NonNull EventBus bus) {
+        // Flying? -> Hovering()
+        // Not flying? -> OnGround()
 
+        Aircraft aircraft = DJIManager.getAircraftInstance();
+        FlightController flightController;
+
+        if(aircraft != null) {
+            flightController = aircraft.getFlightController();
+
+            if (flightController != null) {
+                if(flightController.getState().isFlying()) {
+                    DJIManager.changeOperationMode(new Hovering());
+                } else {
+                    DJIManager.changeOperationMode(new OnGround());
+                }
+            }
+        }
     }
 
 
--- code/ADDS-DJI/app/src/main/java/com/andreasmenzel/adds_dji/OperationModes/OnGround.java
+++ code/ADDS-DJI/app/src/main/java/com/andreasmenzel/adds_dji/OperationModes/OnGround.java
@@ -2,6 +2,13 @@ package com.andreasmenzel.adds_dji.OperationModes;
 
 import androidx.annotation.NonNull;
 
+import com.andreasmenzel.adds_dji.Managers.DJIManager;
+
+import org.greenrobot.eventbus.EventBus;
+
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
+
 /**
  * This Operation Mode states that the drone is currently on the ground.
  *
@@ -15,6 +22,25 @@ public class OnGround extends OperationMode {
     public OnGround() {
         super();
         state = States.active;
+    }
+
+
+    @Override
+    public void perform(@NonNull EventBus bus) {
+        // Not flying? -> OnGround()
+
+        Aircraft aircraft = DJIManager.getAircraftInstance();
+        FlightController flightController;
+
+        if(aircraft != null) {
+            flightController = aircraft.getFlightController();
+
+            if (flightController != null) {
+                if(flightController.getState().isFlying()) {
+                    DJIManager.changeOperationMode(new Hovering());
+                }
+            }
+        }
     }
 
 
